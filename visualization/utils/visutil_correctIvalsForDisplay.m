9e5f8f6df1956350d960dab3faa1627477a33978
Sven Daehne <sven.daehne@tu-berlin.de>
2012-09-19 22:32:02 +0200

    converted to new toolbox convention, i.e. type checking and new optional argument structure. Proper commenting is still missing though...

--- visualization/utils/visutil_correctIvalsForDisplay.m
+++ visualization/utils/visutil_correctIvalsForDisplay.m
@@ -1,20 +1,26 @@
 function ival= visutil_correctIvalsForDisplay(ival, varargin)
 
 if length(varargin)==1,
-  varargin= {'fs', varargin{1}};
+  varargin= {'Fs', varargin{1}};
 end
-opt= propertylist2struct(varargin{:});
-opt= set_defaults(opt, ...
-                  'fs', 100, ...
-                  'sort', 1, ...
-                  'only_point_intervals', 0);
 
-if opt.sort,
+misc_checkType(ival,'DOUBLE[- 2]');
+
+props= {'Fs'                       100       'DOUBLE[1]'
+        'Sort'                      1        'BOOL'
+        'OnlyPointIntervals'        0        'BOOL'   };
+
+opt= opt_proplistToStruct(varargin{:});
+[opt, isdefault]= opt_setDefaults(opt, props);
+opt_checkProplist(opt, props);
+
+
+if opt.Sort,
   [so,si]= sort(ival(:,1));
   ival= ival(si,:);
 end
 
-if opt.only_point_intervals,
+if opt.OnlyPointIntervals,
   to_be_checked= find(diff(ival, 1, 2)==0)';
 else
   to_be_checked= 1:size(ival, 1);
@@ -22,9 +28,9 @@ end
 
 for ii= to_be_checked,
   if ii==1 | ival(ii-1,2)<ival(ii,1),
-    ival(ii,1)= ival(ii,1) - 1000/opt.fs/2;
+    ival(ii,1)= ival(ii,1) - 1000/opt.Fs/2;
   end
   if ii==size(ival,1) | ival(ii,2)<ival(ii+1,2),
-    ival(ii,2)= ival(ii,2) + 1000/opt.fs/2;
+    ival(ii,2)= ival(ii,2) + 1000/opt.Fs/2;
   end
 end
