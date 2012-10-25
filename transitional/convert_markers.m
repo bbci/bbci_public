1c8b69aa3d19d1f4dd0c384f894f48b1db1b0949
Benjamin Blankertz <benjamin.blankertz@tu-berlin.de>
2012-10-25 12:05:40 +0200

    corrected an error reported by Markus that occurs when the old marker has a field time.

--- transitional/convert_markers.m
+++ transitional/convert_markers.m
@@ -1,8 +1,6 @@
 function mrk= convert_markers(mrk_old)
 
 mrk= rmfield(mrk_old, {'pos','toe','fs'});
-mrk.time= mrk_old.pos/mrk_old.fs*1000;
-mrk.desc= mrk_old.toe;
 
 if isfield(mrk, 'indexedByEpochs'),
   mrk= rmfield(mrk, mrk.indexedByEpochs);
@@ -12,3 +10,6 @@ if isfield(mrk, 'indexedByEpochs'),
     mrk.event= setfield(mrk.event, fld, mrk_old.(fld));
   end
 end
+
+mrk.time= mrk_old.pos/mrk_old.fs*1000;
+mrk.desc= mrk_old.toe;
