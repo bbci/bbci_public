e7260ccb4296ae688648425f3ef439e1cfd44707
MichaelTangermann <ms@we-are.de>
2014-01-27 18:32:35 +0100

    list of utility functions contained in this folder

---
+++ classification/utils/Contents.m
@@ -0,0 +1,6 @@
+% This folder contains scripts/utilities to support the functionality of classifiers
+%
+%
+%CLSUTIL_SHRINKAGE - Determine shrinkage parameter of a classifier. 
+%                    Example: the degree of regularization of the estimated covariance matrix 
+%                    towards a unit sphere (used in LDA classifiers)
