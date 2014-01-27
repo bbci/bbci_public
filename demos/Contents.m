1e3760d8825e58cbed5ed21822fef45088217528
Sven Daehne <sven.daehne@tu-berlin.de>
2014-01-27 10:34:49 +0100

    Added Contents.m for demo files

---
+++ demos/Contents.m
@@ -0,0 +1,14 @@
+%This directory contains scripts that demonstrate the basic functionality
+%of this toolbox.
+%
+%
+%DEMO_ANALYSIS_ERPS - Demo script for basic analysis of Event Related
+%                       Potentials (ERPs).
+%DEMO_ANALYSIS_GA_ERPS - Demo script for basic analysis of grand average 
+%                       (i.e. averages across subjects) of Event Related
+%                       Potentials (ERPs).
+%DEMO_ANALYSIS_ERD - Demo script for basic analysis of Event Related
+%                       Desynchronisation (ERD).
+%DEMO_ANALYSIS_SPECTRA - Demo script for spectral analysis.
+%DEMO_CONVERT_CENTERSPELLERMVEP_DATASETS - Demo script for converting raw
+%                                       EEG files to matlab format. 
