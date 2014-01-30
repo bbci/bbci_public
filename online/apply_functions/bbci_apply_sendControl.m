0567b84655304cca6172cc3b1d9fb2c16d632e6d
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-01-30 09:35:00 +0100

    delete code that was erroneously introduced (feedback_receiver='udp', 'osc')

--- online/apply_functions/bbci_apply_sendControl.m
+++ online/apply_functions/bbci_apply_sendControl.m
@@ -19,17 +19,11 @@ if isequal(control_signal, 'init'),
   switch(bbci_feedback.receiver),
    case '',
     % do nothing
-   case 'udp',
-    send_data_udp(bbci_feedback.host, bbci_feedback.port);
-%      data_feedback.state.socket = pnet('udpsocket', 99999);
-%      pnet(data_feedback.state.socket, 'udpconnect', bbci_feedback.host, bbci_feedback.port);
    case 'pyff',
 %    send_udp_xml('init', bbci_feedback.host, bbci_feedback.port);
    case 'tobi_c',
     send_tobi_c_udp('init', bbci_feedback.host, bbci_feedback.port);
-   case 'osc',
-    data_feedback = send_osc_udp('init', bbci_feedback, data_feedback);
-   case 'matlab',
+    case 'matlab',
     if isfield(bbci_feedback, 'opt'),
       data_feedback.opt= bbci_feedback.opt;
     else
@@ -45,17 +39,11 @@ end
 if isequal(control_signal, 'close'),
   switch(bbci_feedback.receiver),
    case 'pyff',
-   case 'udp',
-    send_data_udp;
-% %     pnet(data_feedback.state.socket, 'close');
-% %     data_feedback.state = rmfield(data_feedback.state, 'socket');
 %    We do not close this channel, as it is probably still required to
 %    send signals to Pyff.
 %    send_udp_xml('close');
    case 'tobi_c',
     send_tobi_c_udp('close');
-   case 'osc',
-    data_feedback = send_osc_udp('close');
   end
   return;
 end
@@ -68,19 +56,9 @@ switch(bbci_feedback.receiver),
    if ~isempty(control_signal),
      send_udp_xml(control_signal{:});
    end
- case 'udp',
-   if ~isempty(control_signal),
-     send_data_udp(control_signal{2});
-%      pnet(data_feedback.state.socket, 'write',double(control_signal{2}),'intel');
-%      pnet(data_feedback.state.socket, 'writepacket');
-   end
  case 'tobi_c',
   if ~isempty(control_signal),
     send_tobi_c_udp('send', control_signal{2}(1));
-  end
- case 'osc',
-  if ~isempty(control_signal),
-    data_feedback = send_osc_udp('send', control_signal, data_feedback);
   end
  case 'matlab',
   idx= find(cellfun(@(x)isequal(x,'cl_output'), control_signal));
