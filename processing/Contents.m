ec1321233a8072ec695d0702985c912d2edc57f5
Johannes <johannes@TOBI-Johannes.(none)>
2014-02-10 13:29:59 +0100

    processing contents added

---
+++ processing/Contents.m
@@ -0,0 +1,11 @@
+% This directory contains the files which can be used for various ways for
+% data processing (offline and online) and analyses.
+%
+%PROC_APPENDCHANNELS - Append a channel to continuous or epoched data
+%PROC_APPENDCNT - Append Continuous EEG Data
+%PROC_APPENDEPOCHS - appends two or more epochs.
+%PROC_APPENDSAMPLES - append samples of data structs
+%PROC_AUCVALUES - Computes the area under the curve (AUC) score for each feature
+%PROC_AVERAGE - Classwise calculated averages
+
+
