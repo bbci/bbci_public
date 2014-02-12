18cfdc519104477850c7430f416a3cf131d77975
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-02-13 00:18:43 +0100

    renamed bbci online demos to include prefix tag 'bbcionline'

---
+++ online/demos/demo_bbcionline_calibrate_and_apply_ERP_Speller.m
@@ -0,0 +1,46 @@
+BC= [];
+BC.fcn= @bbci_calibrate_ERP_Speller;
+BC.settings.nClasses= 6;
+BC.file= 'VPibq_11_05_18/calibration_CenterSpellerFixedSequenceVPibq';
+BC.read_param= {'fs',100};
+BC.marker_fcn= @mrk_defineClasses;
+BC.marker_param= {{[31:49], [11:29]; 'target', 'nontarget'}};
+
+% In demos, we just write to the temp folder. Otherwise, the default
+% choice would be fine.
+BC.save.folder= BBCI.TmpDir;
+BC.log.folder= BBCI.TmpDir;
+
+bbci= struct('calibrate', BC);
+
+[bbci, calib]= bbci_calibrate(bbci);
+%bbci_save(bbci, calib);
+
+
+% test consistency of classifier outputs in simulated online mode
+bbci.source.acquire_fcn= @bbci_acquire_offline;
+bbci.source.acquire_param= {calib.cnt, calib.mrk};
+
+bbci.log.output= 'screen&file';
+bbci.log.folder= BBCI.TmpDir;
+bbci.log.classifier= 1;
+
+data= bbci_apply_uni(bbci);
+
+log_format= '%fs | M(%u) | %fs | [%f] | %s';
+[time, marker_desc, marker_time, cfy, control]= ...
+    textread(data.log.filename, log_format, ...
+             'delimiter','','commentstyle','shell');
+
+isequal(marker_desc, calib.mrk.desc')
+
+ref_ival= bbci.feature.proc{1}{2};
+cfy_ival= bbci.feature.proc{2}{2};
+epo= proc_segmentation(calib.cnt, calib.mrk, bbci.feature.ival, ...
+                       'clab', bbci.signal.clab);
+fv= proc_baseline(epo, ref_ival);
+fv= proc_jumpingMeans(fv, cfy_ival);
+out= applyClassifier(fv, 'LDA', bbci.classifier.C);
+
+% validate classifier outputs of simulated online and offline processing
+max(out(:)- cfy)
---
+++ online/demos/demo_bbcionline_matlab_feedback.m
@@ -0,0 +1,57 @@
+% BBCI DEMO - Testing Matlab feedback using random signals.
+%
+%  The demo shows, how a Matlab-based feedback can be tested in simulated
+%  online mode. As a source of signals, a random signal generator is used
+%  (acquire fcn 'bbci_acquire_randomSignals').
+%  For this demo, the data processing does not matter. Here, we define
+%  a very simple processing chain with a random clasifiers.
+
+
+clab= {'C3','Cz','C4', 'CP3','CPz','CP4'};
+C= struct('b', 0);
+C.w= randn(length(clab), 1);
+
+bbci= struct;
+bbci.source.acquire_fcn= @bbci_acquire_randomSignals;
+bbci.source.acquire_param= {'clab',clab, 'realtime', 0.5};
+bbci.feature.proc= {@proc_variance, @proc_logarithm};
+bbci.feature.ival= [-500 0];
+bbci.classifier.C= C;
+bbci.quit_condition.marker= 255;
+
+bbci.feedback.receiver= 'matlab';
+bbci.feedback.fcn= @bbci_feedback_cursor;
+bbci.feedback.opt= ...
+    struct('trigger_classes_list', {{'left','right'}}, ...
+           'countdown', 3000, ...
+           'trials_per_run', 6);
+bbci.feedback.log.output= 'file';
+bbci.feedback.log.folder= BBCI.TmpDir;
+
+data= bbci_apply_uni(bbci);
+
+pause(1); close;
+fprintf('Now doing a replay of that feedback from the logfile.\n'); pause(2);
+
+bbci_fbutil_replay(data.feedback.log.filename);
+
+% Replay in time lapse:
+%bbci_fbutil_replay(data.feedback.log.filename, 'realtime',0);
+
+
+return
+
+
+%% SANDBOX - test running TWO feedbacks simultaneously and logging
+
+bbci.feedback.log.output= 'file';
+bbci.feedback.log.folder= '/tmp/';
+
+bbci.feedback(2)= bbci.feedback;
+bbci.feedback(2).opt.fig= 2;
+bbci.feedback(2).opt.geometry= BBCI.Tp.Geometry + [BBCI.Tp.Geometry(3) 0 0 0];
+bbci.feedback(2).opt.trigger_fcn= '';
+
+bbci.quit_condition.marker= 255;
+
+data= bbci_apply(bbci);
