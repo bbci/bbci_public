07af01ac5dd48ae0946e2f37bbeb7d24f764cda0
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-02-15 02:52:52 +0100

    Jan's functions for NIRS analysis

---
+++ processing/proc_BeerLambert.m
@@ -0,0 +1,124 @@
+function dat = nirs_LB(dat, varargin)
+% Synopsis:
+%   DAT = nirs_LB(DAT, 'Property1',Value1, ...)
+%
+% Arguments:
+%   DAT: data struct with NIRS cnt data split in the x field
+%
+%   OPT - struct or property/value list of optional properties:
+%   'Citation'   - if set, the epsilon (extinction values) is taken from
+%                  the specified citation number (see GetExtinctions.m for
+%                  details). (default 1 = Gratzer et al)
+%   'Epsilon'    - sets extinction coefficients manually (wl1 wl2 vs deoxy
+%                  and oxy). In this case, citation is ignored. State in
+%                  millimol/liter(?).
+%   'Opdist'     - optode (source-detector) distance in cm (default 2.5)
+%   'Ival'       - either 'all' (default) or a vector [start end] in samples
+%                  specifying the baseline for the LB transformation
+%   'DPF'        - differential pathlength factor: probabilistic average
+%                  distance travelled by photons, default [5.98 7.15]
+%  
+% Returns:
+%   DAT - updated data struct with oxy/deoxy fields specifying absorption
+%         values in mmol/l.
+%
+%
+% See also: nirs_* nirsfile_* GetExtinctions
+% 
+% Note: Based on the nirX Nilab toolbox functions u_LBG and u_popLBG.
+%
+%
+% matthias.treder@tu-berlin.de 2011, mail@aewald.net 2013
+% AE: changed channels label from wl1&wl2 to oxa and deoxy.
+% Markus Wenzel 2013 (adapted it to the new toolbox)
+% Jan Mehnert February 2014 (ready for public BBCI toolbox) (jan@mehnert.org)
+% ToDo: possibility to define individual baseline; online functionability
+
+global BBCI
+props={'Citation'	1           'INT'
+        'Opdist'	2.5         'DOUBLE'
+        'Ival'      'all'       'CHAR|DOUBLE'
+        'DPF'       [5.98 7.15] 'DOUBLE'
+        'Epsilon'   []          'DOUBLE'
+        'Verbose'	0           'BOOL'};
+
+if nargin==0,
+    dat= props; return
+end
+
+opt= opt_proplistToStruct(varargin{:});
+[opt, isdefault]= opt_setDefaults(opt, props);
+
+opt_checkProplist(opt, props);
+misc_checkType(dat, 'STRUCT');
+
+
+if strcmp(opt.Ival,'all')
+  opt.Ival = [1 size(dat.x,1)];
+end
+
+s1=size(dat.x,1);
+s2=size(dat.x,2)/2;
+
+wl1 = dat.x(:,1:end/2) + eps;
+wl2 = dat.x(:,end/2+1:end) + eps;
+
+%% Get epsilon
+if isempty(opt.Epsilon)
+  if ~isfield(dat,'wavelengths')
+    error('Wavelengths should be given in the .wavelengths field.')
+  end
+  [ext,nfo] = procutil_getExtinctions(dat.wavelengths,opt.Citation);
+  if opt.Verbose, fprintf('Citation: %s\n',nfo), end
+  epsilon = ext(:,1:2);
+  
+  % Divide by 1000 to obtain the required unit
+  epsilon = epsilon/1000;
+  
+else
+  epsilon = opt.Epsilon;
+end
+
+%% Arrange epsilon so that higher wavelength is on top
+[mw,idx] = max(dat.wavelengths);
+if idx==2 % higher wavelength is on bottom
+  epsilon = flipud(epsilon);
+  if opt.Verbose, fprintf('Epsilon matrix was rearranged so that higher WL is on top\n'), end
+end
+
+%% Apply Lambert-Beer law
+Att_highWL= real(-log10( wl2 ./ ...
+    ( repmat(mean(wl2(opt.Ival(1):opt.Ival(2),:),1), [s1,1]))   ));
+
+Att_lowWL= real(-log10( wl1./ ...
+    ( repmat(mean(wl1(opt.Ival(1):opt.Ival(2),:),1), [s1,1]))   ));
+
+A=[];
+A(:,1)=reshape(Att_highWL,s1*s2,1);
+A(:,2)=reshape(Att_lowWL,s1*s2,1);
+
+%----------------------------------
+%       3.cc
+%----------------------------------
+% e=...looks like this
+%               oxy-Hb         deoxy-Hb
+% higherWL: 830 | e: 0.974       0.693
+% lowerWL : 690 | e: 0.35         2.1
+
+e= epsilon/10;
+
+e2=   e.* [opt.DPF' opt.DPF']  .*  opt.Opdist;
+c= ( inv(e2)*A'  )';
+
+dat.x    =reshape(c(:,1),s1,s2); %in mmol/l
+dat.x    = [dat.x reshape(c(:,2),s1,s2)]; %in mmol/l
+
+%% Change Channel labels wl1 & wl2 to 'oxy' and 'deoxy'
+
+
+dat.clab = strrep(dat.clab, 'highWL', 'oxy');
+dat.clab = strrep(dat.clab, 'lowWL', 'deoxy');
+
+dat.signal = 'NIRS (oxy, deoxy)';
+
+dat.YUnit = 'mmol/l';
---
+++ visualization/mnt_restrictNIRSMontage.m
@@ -0,0 +1,137 @@
+function mnt = mnt_restrictNIRSMontage(mnt,varargin)
+% mnt_restrictNIRSMontage - restricts the NIRS montage by selecting (a)
+%       specified NIRS channels and/or (b) sources or detectors and 
+%       keeping only the corresponding NIRS channels AND/OR (c) keeping only 
+%       informative NIRS channels (ie channels corresponding to a relatively 
+%       small source-detector distance).
+%
+% Synopsis:
+%   MNT = mnt_restrictNIRSMontage(MNT, CHANS, <OPT>)
+%   MNT = mnt_restrictNIRSMontage(MNT, <OPT>)
+%
+% Properties:
+%    mnt             -  NIRS montage
+%    chans           -  restrict montage to these NIRS channels (can
+%                       contain any wildcards * and #; see chanind). 
+%                       Must be a cell array of strings.
+%
+% OPT - struct or property/value list of optional properties:
+%   'Source':  a cell array containing the labels or physical numbers of
+%              sources that are to be selected. All other sources and NIRS
+%              channels containing these sources are removed. Default {}
+%              (ie all sources are considered).
+%              The cell array can also contain the keyword 'not' as first
+%              element, in which the selection is inverted (ie {'not'
+%              'Fz'} would remove Fz and preserve all *other* sources).
+%   'Detector':  The same as 'Source' for the detectors.
+%   'Dist'  :  gives the maximum distance [in cm] between source/detector pairs. 
+%              If set, source/detector pairs with a larger distance are
+%              removed.  Assuming a head radius of default 10 cm (set 'headRadius').
+%              The default value of dist is 3.5 - if you do not want
+%              channels to be reduced at all according to distance, set
+%              dist to [].
+%   'RemoveOptodes' : if 1, the non-selected optodes are not only removed
+%              from the NIRS channels (mnt.clab field) but also from the
+%              corresponding source and detector fields (mnt.source and
+%              mnt.detector). (default 1)
+%
+% OUT:  mnt             -  updated montage
+%
+% Note: Use proc_selectChannels to reduce the NIRS data (cnt,dat,epo) 
+% according to the new montage.
+%
+% See also: mnt_restrictMontage
+%
+% matthias.treder@tu-berlin 2011
+% Markus Wenzel 2013 (adapted it to the new toolbox)
+% Jan Mehnert February 2014 (ready for public BBCI toolbox) (jan@mehnert.org)
+
+%% Check which variant of the function is used
+global BBCI
+props={ 'Chans'         {}      'CHAR|CELL{CHAR}'
+        'Source'        {}      'CHAR|CELL{CHAR}'
+        'Detector'      {}      'CHAR|CELL{CHAR}'
+        'RemoveOptodes' 1       'BOOL'
+        'HeadRadius'    10      'DOUBLE'
+        'Dist'          3.5     'DOUBLE'};
+                 
+if nargin==0,
+    mnt= props; return
+end
+
+if nargin>1 && mod(nargin,2)==0 %% first varargin is CHANS
+  if numel(varargin)>1
+    opt = opt_proplistToStruct(varargin{2:end});
+    opt.Chans = varargin{1};
+  elseif isstruct(varargin{1})
+    opt = varargin{1};
+  else
+    opt = struct();
+    opt.Chans = varargin{1};
+  end
+else
+  opt = opt_proplistToStruct(varargin{:});
+end
+
+[opt, isdefault]= opt_setDefaults(opt, props);
+opt_checkProplist(opt, props);
+misc_checkType(mnt, 'STRUCT');
+
+
+if ischar(opt.Source)
+  opt.Source = {opt.Source};
+end
+if ischar(opt.Detector)
+  opt.Detector = {opt.Detector};
+end
+if ischar(opt.Chans)
+  opt.Chans = {opt.Chans};
+end
+                
+%% Select sources and/or detectors
+if ~isempty(opt.Source)
+  selSou = mnt.source.clab(util_chanind(mnt.source,opt.Source));
+  if opt.RemoveOptodes
+    mnt.source = mnt_restrictMontage(mnt.source,selSou);
+  end
+end
+if ~isempty(opt.Detector)
+  selDet = mnt.detector.clab(util_chanind(mnt.detector,opt.Detector));
+  if opt.RemoveOptodes
+    mnt.detector = mnt_restrictMontage(mnt.detector,selDet);
+  end
+end
+
+%% Find connector for source-detector labels (non-alphanumeric character)
+str = str_head(mnt.clab);
+[a,a,a,connector] = regexp(str,'[^\w]');
+connector = cell2mat(unique(cell_flaten(connector)));
+if isempty(connector)
+  connector = '';
+elseif numel(connector)>1
+  error('Multiple connectors in NIRS clab: [%s]',[connector{:}])
+end
+
+%% Select specified NIRS channels
+if ~isempty(opt.Chans)
+  mnt = mnt_restrictMontage(mnt,opt.Chans,{'ignore' connector}); 
+end
+
+%% Restrict NIRS channels by removing the deleted sources/detectors
+if ~isempty(opt.Source) || ~isempty(opt.Detector)
+  if isempty(opt.Source), selSou = '*'; end
+  if isempty(opt.Detector), selDet = '*'; end
+  if ~iscell(selSou), selSou = {selSou}; end
+  % Build selection string
+  sel = strcat(selSou,connector);
+  sel = cell_flaten(apply_cellwise(sel,'strcat',selDet));
+  mnt = mnt_restrictMontage(mnt,sel);
+end
+
+
+%% Reduce NIRS channels according to source-detector distance
+if ~isempty(opt.Dist)
+  dist = mnt.angulardist * opt.HeadRadius;   % distances in cm
+  sel = find(dist<opt.Dist);  
+  mnt = mnt_restrictMontage(mnt,sel);
+end
