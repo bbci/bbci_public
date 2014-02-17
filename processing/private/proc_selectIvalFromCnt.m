8bf34f7771f831880f63de22b7624933dd4dc095
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-02-17 15:40:37 +0100

    new function proc_selectIvalFromCnt.m

---
+++ processing/private/proc_selectIvalFromCnt.m
@@ -0,0 +1,26 @@
+function [cnt, mrk]= proc_selectIval(cnt, mrk, ival)
+%PROC_SELECTIVAL - Select subinterval from epoched or continuous data
+%
+%Synopsis:
+% [CNT, MRK]= proc_selectIvalFromCnt(CNT, MRK, IVAL)
+%
+% Selects the time segment given by ival ([start_ms end_ms]). You may
+% use inf as end_ms.
+%
+%Arguments:
+% EPO,CNT - data structure of continuous (with markers MRK) or epoched data
+% IVAL - time segment to be extracted
+%  
+%Returns:
+% CNT,MRK  - updated data structures
+
+ival_sa= round(ival*cnt.fs/1000);
+if ival_sa(1)==0,
+  ival_sa(1)= 1;
+end
+if isinf(ival_sa(2)),
+  ival_sa(2)= size(cnt.x,1);
+end
+
+cnt.x= cnt.x(ival_sa(1):ival_sa(2),:);
+mrk.time= mrk.time - ival(1);
