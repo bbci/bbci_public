1bb369e1e04086f74742c204b091e1199c9257f5
Johannes <johannes@TOBI-Johannes.(none)>
2014-02-18 17:16:56 +0100

    Michaels changes were undone

--- processing/proc_filtfilt.m
+++ processing/proc_filtfilt.m
@@ -1,9 +1,8 @@
-function dat= proc_filtfilt(dat,varargin)
+function dat= proc_filtfilt(dat,b, a)
 %PROC_FILTFILT - Zero-phase forward and reverse digital filtering
 %
 %Synopsis:
 % DAT= proc_filtfilt(DAT, B, A)
-% DAT= proc_filtfilt(DAT, z, p, k)
 %
 % Apply digital (FIR or IIR) filter forward and backward
 % -> zero-phase filter. This filtering is not causal!
@@ -12,9 +11,6 @@ function dat= proc_filtfilt(dat,varargin)
 % DAT   - data structure of continuous or epoched data
 % B,A   - filter coefficients
 %
-%Arguments(2):
-% DAT   - data structure of continuous or epoched data
-% z,p,k - zeros, poles and gain factor
 %
 %Returns:
 % DAT   - updated data structure
@@ -26,42 +22,7 @@ function dat= proc_filtfilt(dat,varargin)
 % % Apply a zero-phase band-pass filter 7 to 13Hz to cnt:
 % cnt_flt= proc_filtfilt(cnt, b, a);
 %
-%Example 2: Highpass filter above 6 Hz
-% % Due to desired high attenuation and small drop-off band (4 Hz to 6 Hz) 
-% % and resulting large filter order, the above way of defining a filter via B,A is
-% % not possible any more. Use different representation including a gain k:
-% db_attenuation = 60;
-% Wps = [6 4]/cnt.fs*2;
-% [n, Wn]= cheb2ord(Wps(1), Wps(2) , 3, db_attenuation);
-% [z,p,k] = cheby2(n, db_attenuation, Wn);        
-% cnt_flt= proc_filtfilt(cnt, z,p,k);
-%
-% 
-% Please Note (see cheby2.m): 
-% "In general, you should use the [z,p,k] syntax to design IIR filters. 
-% For higher order filters (possibly starting as low as order 8),   
-% numerical problems due to roundoff errors may occur when forming the 
-% transfer function using the [b,a] syntax." 
-%
-% z,p,k functionality added by David List, Michael Tangermann 26.April 2010
-% based on a looted script from matlab central (filtfilthd.m)
 %
 %See also proc_filt.
-dat = misc_history(dat);
 
-if size(varargin,2) == 1
-  varargin= cat(2, varargin, {1});
-end
-
-if size(varargin,2) == 2
-    % varargin(1) == B
-    % varargin(2) == A
-    dat.x(:,:)= filtfilt(varargin{1}, varargin{2}, dat.x(:,:));
-elseif size(varargin,2) == 3 
-    % varargin(1) == z
-    % varargin(2) == p
-    % varargin(3) == k
-    [sos_var,g] = zp2sos(varargin{1}, varargin{2}, varargin{3});
-    Hd  = dfilt.df2sos(sos_var, g);
-    dat.x(:,:) = filtfilthd(Hd,dat.x(:,:));
-end
+dat.x(:,:)= filtfilt(b, a, dat.x(:,:));
