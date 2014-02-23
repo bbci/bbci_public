32773f15ad7f484c60e5d2b8a9151cc40f52e114
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2014-02-23 13:06:12 +0100

    corrected filename in ERP_MI demo and made color order defaults

--- visualization/utils/defopt_erps.m
+++ visualization/utils/defopt_erps.m
@@ -2,11 +2,12 @@ function opt= defopt_erps(varargin)
 %DEFOPT_ERPS - Properties for plot_channel* functions, optimized for
 %plotting ERPs
 
-props= {'LineWidth',                    0.7;
-        'AxisType',                     'cross';
-        'AxisTitleVerticalAlignment',   'top';
-        'AxisTitleFontWeight',          'demi';
+props= {'LineWidth',                    0.7
+        'AxisType',                     'cross'
+        'AxisTitleVerticalAlignment',   'top'
+        'AxisTitleFontWeight',          'demi'
         'ScaleHPos',                    'left'
+        'ColorOrder',             [1 .47 .02; .3 .3 .3; 0 .6 .8; .9 0 .9], 
         };
 
 opt= opt_proplistToStruct(varargin{:});
