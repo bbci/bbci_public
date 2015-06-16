5fad694a9f6346e48d6b764522941b93a39441fa
Mihail Bogojeski <mihail.bogojeski@campus.tu-berlin.de>
2015-06-16 17:15:58 +0200

    Changed signatures of both functions so ival is not used anymore. Signature of apply_HLDA should now be compatible with cross validation. Added optional parameter 'Regression' in train_HLDA, which can be used to specify logistic regression as the top-level classifier for HLDA.

--- classification/apply_HLDA.m
+++ classification/apply_HLDA.m
@@ -1,16 +1,13 @@
-function out = apply_HLDA(epo, ival, C)
-% TRAIN_LDA - linear discriminant analysis 
+function out = apply_HLDA(XTr, C)
+% APPLY_HLDA - Hierarchical linear discriminant analysis 
 %
 %Synopsis:
-%   out = apply_HLDA(epo, ival, C)
+%   out = apply_HLDA(XTr, C)
 % 
 %Arguments:
-%   epo: STRUCT         - Structure containing epoched ERP data, with
-%                           obligatory fields 'x', 'y' and 't'.
-%   ival: INT [2]       - interval of the ERP on which the training should be
-%                           performed. Must match the
-%                           size of the interval used to train the
-%                           classifier C.
+%   XTR: DOUBLE [TxNxM] - Data matrix, with T temporal features, N
+%                           N spatial features, and M 
+%                           training points/examples. 
 %   C: STRUCT           - a hierarchical LDA classifier structure that 
 %                           contains the individual segment classifiers and 
 %                           the top-level classifier. Must include the
@@ -19,30 +16,26 @@ function out = apply_HLDA(epo, ival, C)
 %   out: FLOAT[]        - an array containing the classifier score for each 
 %                           sample
 %Description:
-%   APPLY_HLDA applies a hierarchical LDA classifier given an epo structure,
-%               a training interval and a number of segments
+%   APPLY_HLDA applies a hierarchical LDA classifier given data and a
+%   trained HLDA classifier.
+%
+%   References:Gerson, A.D., Parra, L.C., Sajda, P.: Cortically coupled 
+%   computer vision for rapid image search. IEEE Transactions on Neural 
+%   Systems and Rehabilitation Engineering 14, 174–179 (2006).
 %
 %Examples:
-%   apply_HLDA(epo, ival, C))
+%   apply_HLDA(XTr, C))
 %   
 %See also:
 %   TRAIN_HLDA
 
-misc_checkType(epo, 'STRUCT(x y t)');
-misc_checkType(ival, 'INT[2]');
+misc_checkType(XTr, 'DOUBLE');
 misc_checkType(C, 'STRUCT(seg final)');
 
 
 nSegments = length(C.seg);
-dims = size(epo.x);
-% convert interval values to indices
-ival_idx = find(epo.t == ival(1)) : find(epo.t == ival(2));
 
-% extract relevant interval from data
-epo.x = epo.x(ival_idx, :, :);
-
-
-dims = size(epo.x);
+dims = size(XTr);
 
 %boundary indices between segments
 seg_idx = round(linspace(0, dims(1), nSegments+1));
@@ -50,12 +43,19 @@ seg_idx = round(linspace(0, dims(1), nSegments+1));
 seg_scores = zeros(nSegments, dims(end));
 
 for i = 1:nSegments
-    seg = epo.x(seg_idx(i) + 1 : seg_idx(i + 1), :, :); % i-th segment of X
+    seg = XTr(seg_idx(i) + 1 : seg_idx(i + 1), :, :); % i-th segment of X
     %apply LDA classifier for this segment
     seg = reshape(seg, [], dims(end));
     seg_scores(i,:) = apply_separatingHyperplane(C.seg(i), seg); %get classifier scores for segment
 end
 
-trial_scores = apply_separatingHyperplane(C.final, seg_scores);
-
-out = trial_scores;
+if isfield(C.final, 'B')
+    %logistic regression as top-level classifier
+    exps = C.final.B(1) + C.final.B(2:end)'*seg_scores;
+    out = 1./(1 + exp(-exps));
+    %transform scores to interval [-1 1]
+    out = -2*(out - 0.5);
+else
+    %LDA as top-level classifier
+    out = apply_separatingHyperplane(C.final, seg_scores);
+end
