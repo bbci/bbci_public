369c4a6f97bb2b3206cd70f51668ad2d7c2901ef
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2015-08-12 17:44:08 +0200

    Replaced acq_checkTriggers.m by a corrected version. Closes issue #67. Also closes issue #59.

--- acquisition/tools/acq_checkTriggers.m
+++ acquisition/tools/acq_checkTriggers.m
@@ -8,27 +8,29 @@ function acq_checkTriggers(bbci)
 %which is (currently) BrainVision.
 
 
-misc_checkTypeifExists('bbci', 'STRUCT');
+misc_checkTypeIfExists('bbci', 'STRUCT');
 
 bbci= bbci_apply_setDefaults(bbci);
 bbci.source.record_signals= 0;
 [data, bbci]= bbci_apply_initData(bbci);
+pause(0.5);
 
-inp= 2^[0:7];
+inp= 2.^[0:7];
 trigger_ok= 1;
-for k= 1:8,
+for k= 1:length(inp),
   fprintf('sending trigger: %3d -> received: ', inp(k));
-  outp{k}= acq_checkTrigger(bbci, inp(k), 500);
+  outp{k}= acq_checkTrigger(bbci, data, inp(k), 20);
   if isempty(outp{k}),
     fprintf('nothing.\n');
   else
-    fprintf('%3d.\n');
+    fprintf('%3d.\n', outp{k});
   end
   if ~isequal(inp(k), outp{k}),
     trigger_ok= 0;
   end
 end
 
+bbci_apply_close(bbci);
 if trigger_ok,
   fprintf('Trigger test successful.\n');
 else
@@ -37,17 +39,19 @@ end
 return
 
 
-function outp= acq_checkTrigger(bbci, inp, timeout)
+function outp= acq_checkTrigger(bbci, data, inp, timeout)
 
 outp= [];
 trigger_received= false;
 t0= clock;
+bbci_trigger(bbci, inp);
 while isempty(outp) && etime(clock, t0)*1000<timeout,
   [source, marker]= ...
         bbci_apply_acquireData(data.source, bbci.source, data.marker);
   if ~source.state.running,
     break;
   end
-  outp= marker.desc;
+  if length(marker.desc)>1,
+    outp= marker.desc(end);
+  end
 end
-bbci_apply_close(bbci);
