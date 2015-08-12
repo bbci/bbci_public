d7939b7f5412b3d383852375b802dc19e77d3f25
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2015-08-12 16:39:47 +0200

    Reestablished original functionality of mrk_mergeMarkers. Hopyfully closes #98.

--- markers/mrk_mergeMarkers.m
+++ markers/mrk_mergeMarkers.m
@@ -21,7 +21,33 @@ end
 
 mrk= mrkutil_appendEventInfo(mrk1, mrk2);
 mrk.time= cat(2, mrk1.time(:)', mrk2.time(:)');
-mrk.y= cat(2, mrk1.y, mrk2.y);
+
+%% Labels (mrk.y) and class names (mrk.className)
+if xor(isfield(mrk1, 'y'), isfield(mrk2, 'y')),
+  error('either none or both marker structres must have field ''y''.');
+end 
+if isfield(mrk1, 'y'),
+  s1= size(mrk1.y);
+  s2= size(mrk2.y);
+  if isfield(mrk1, 'className') && isfield(mrk2, 'className'),
+    mrk.y= [mrk1.y, zeros(s1(1), s2(2))];
+    mrk2y= [zeros(s2(1), s1(2)), mrk2.y];
+    mrk.className= mrk1.className;
+    for ii = 1:length(mrk2.className)
+      c = find(strcmp(mrk.className,mrk2.className{ii}));
+      if isempty(c)
+        mrk.y= cat(1, mrk.y, zeros(1,size(mrk.y,2)));
+        mrk.className=  cat(2, mrk.className, {mrk2.className{ii}});
+        c= size(mrk.y,1);
+      elseif length(c)>1,
+        error('multiple classes have the same name');
+      end
+      mrk.y(c,end-size(mrk2.y,2)+1:end)= mrk2.y(ii,:);
+    end
+  else
+    mrk.y= [[mrk1.y; zeros(s2(1), s1(2))], [zeros(s1(1), s2(2)); mrk2.y]];
+  end
+end
 
 %% Recursion
 if length(varargin)>0,
