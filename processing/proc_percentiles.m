59b377e7f267c5a42a98d747dd0a977834d1912d
DanielMiklody <Daniel.Miklody@gmx.de>
2015-10-29 10:50:49 +0100

    adding functionality (to calculate median only) and improved documentation

--- processing/proc_percentiles.m
+++ processing/proc_percentiles.m
@@ -2,8 +2,8 @@ function out= proc_percentiles(epo, p, varargin)
 %PROC_PERCENTILES - Classwise calculated percentiles
 %
 %Synopsis:
-% EPO= proc_percentiles(EPO, <OPT>)
-% EPO= proc_percentiles(EPO, CLASSES)
+% EPO= proc_percentiles(EPO, p, <OPT>)
+% EPO= proc_percentiles(EPO, p, CLASSES)
 %
 %Arguments:
 % EPO -      data structure of epoched data
@@ -13,7 +13,8 @@ function out= proc_percentiles(epo, p, varargin)
 %            percentile values for the values given here. For scalar p, y is
 %            a row vector containing Pth percentile of each column of X. For
 %            vector p,the ith row of y is the p(i) percentile of each column
-%            of X. For median calculation e.g. p=50;
+%            of X. For quartile calculation e.g. p=[25 75]. If left empty
+%            ([]), only the median is calculated.
 %
 % OPT struct or property/value list of optional arguments:
 %  .Classes - classes of which the average is to be calculated,
@@ -26,19 +27,15 @@ function out= proc_percentiles(epo, p, varargin)
 %
 %Returns:
 % EPO           - updated data structure with fields
-%  .x           - classwise percentiles
+%  .x           - classwise median
 %  .N           - vector of epochs per class across which the percentiles were calculated
-%  .percentiles - vector containing the percentiles
+%  .percentiles - struct containing the percentiles (.x) and their values
+%                   (.p)
 
 % Benjamin Blankertz
 % 10-2015 Daniel Miklody
 
-props= {  'Policy'   'mean' '!CHAR(mean nanmean median)';
-          'Classes' 'ALL'   '!CHAR';
-          'Std'      0      '!BOOL';
-          'Stats'      0    '!BOOL';
-          'Bonferroni' 0    '!BOOL';
-          'Alphalevel' []   'DOUBLE'};
+props= {  'Classes' 'ALL'   '!CHAR' };
 
 if nargin==0,
   out = props; return
@@ -104,12 +101,13 @@ out.N= zeros(1, nClasses);
 epo.x= reshape(epo.x, [prod(sz(1:end-1)) sz(end)]);
 for ic= 1:nClasses,
     out.x(:,ic)= stat_percentiles(epo.x(:,evInd{ic}),50);
-    out.percentiles.x(:,ic,:)= stat_percentiles(epo.x(:,evInd{ic}),p);
+    if ~isempty(p)
+        out.percentiles.x(:,ic,:)= stat_percentiles(epo.x(:,evInd{ic}),p);
+    end
 end
 out.percentiles.p=p;
 
 out.x= reshape(out.x, [sz(1:end-1) nClasses]);
 out.percentiles.x= reshape(out.percentiles.x, [sz(1:end-1) nClasses numel(p)]);
-
 out.indexedByEpochs = {}; 
 
