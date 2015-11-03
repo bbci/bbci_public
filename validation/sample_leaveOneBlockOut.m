36b3b1a4a0c8615dabca0e74c097b9f92c61e8e5
MatthiasSchultze-Kraft <schultze-kraft@tu-berlin.de>
2015-11-03 18:14:52 +0100

    included stratification option in sample_leaveOneBlockOut

--- validation/sample_leaveOneBlockOut.m
+++ validation/sample_leaveOneBlockOut.m
@@ -1,22 +1,54 @@
-function [divTr, divTe]= sample_leaveOneBlockOut(label, block_idx)
-%[divTr, divTe]= sample_leaveOneBlockOut(label, block_idx)
+function [divTr, divTe] = sample_leaveOneBlockOut(label, block_idx, strat)
+%SAMPLE_LEAVEONEBLOCKOUT - Sampling function: block divisions
 %
-% IN  label   - class labels, array of size [nClasses nSamples]
-%               where row r indicates membership of class #r.
-%               (0: no member, 1: member)
-%     block_idx - [1 nSamples]-sized array of indices which defines
-%               to which block each sample belongs. This can e.g., be
-%               mrk.blkno, when mrk is obtained from mrk_evenlyInBlocks.
-% 
-% OUT divTr   - divTr{1}{n}: holds the training set for the case when
-%               block with number n is th hold-out set.
-%     divTe   - analogue to divTr, for the test set
+%Synopsis:
+%  [DIVTR, DIVTE] = sample_leaveOneBlockOut(LABEL, BLOCK_IDX, <STRAT>)
+%
+%Arguments:
+% LABEL     - Despensable input argument, required for compability reasons
+% BLOCK_IDX - [1 x nSamples] array of indices which defines to which block
+%             each sample belongs. This can be e.g. mrk.blkno, when mrk is
+%             obtained from mrk_evenlyInBlocks
+% STRAT     - [1 x 2] bool indicating to stratify the samples by
+%             performing a random sampling on the training and/or test
+%             sets, default [0 0]
+%
+%Returns: 
+% DIVTR     - Partitions of the training set
+% DIVTE     - Partitions of the test set
 
+% 2015-11 Matthias Schultze-Kraft
 
-divTr= cell(1, 1);
-divTe= cell(1, 1);
-idx_list= unique(block_idx);
-for nn= 1:length(idx_list),
-  divTe{1}(nn)= {find(block_idx==idx_list(nn))};
-  divTr{1}(nn)= {find(block_idx~=idx_list(nn))};
+if nargin<3
+    strat = [0 0];
 end
+
+divTr = cell(1, 1);
+divTe = cell(1, 1);
+idx_list = unique(block_idx);
+for nn = 1:length(idx_list)
+    % test set indices
+    idx = find(block_idx==idx_list(nn));
+    if strat(2)
+        idx = stratify(idx,label);
+    end
+    divTe{1}(nn) = {idx};
+    % training set indices
+    idx = find(block_idx~=idx_list(nn));
+    if strat(1)
+        idx = stratify(idx,label);
+    end
+    divTr{1}(nn) = {idx};    
+end
+
+function idx = stratify(idx,label)
+idx = {idx(logical(label(1,idx))) idx(logical(label(2,idx)))};
+Nmin = min([length(idx{1}) length(idx{2})]);
+if Nmin==0
+    warning('No stratification performed because the number of samples of one class is zero.')
+else
+    [Nmax,ci] = max([length(idx{1}) length(idx{2})]);
+    ri = randperm(Nmax,Nmin);
+    idx{ci} = idx{ci}(ri);
+end
+idx = [idx{:}];
