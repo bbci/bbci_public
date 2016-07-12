4f268d00d271eb22bb13b608d0a2569d19fb93ff
BenjaminBlankertz <benjamin.blankertz@tu-berlin.de>
2016-07-12 12:45:51 +0200

    minor improvements in grid_plot, closes #191

--- visualization/utils/gridutil_getClabOfGrid.m
+++ visualization/utils/gridutil_getClabOfGrid.m
@@ -1,8 +1,16 @@
-function clab= gridutil_getClabOfGrid(mnt)
+function [clab, idx]= gridutil_getClabOfGrid(mnt)
 %GETCLABOFGRID - Channel names of channels Visible in the grid of a montage
 %
 %Synopsis:
-% CLAB= gridutil_getClabOfGrid(MNT)
+%  [CLAB, IDX]= gridutil_getClabOfGrid(MNT)
+%
+%Arguments:
+%  MNT - structure defining EEG montage and plot layout, 
+%        see mnt_setElectrodePositions and mnt_setGrid
+%
+%Returns:
+%  CLAB - [CELL{CHAR}] labels of channels that are included in the grid layout
+%  idx  - indices (relative to MNT.clab) of those channels
 
 if isfield(mnt, 'box'),
   idx= find(~isnan(mnt.box(1,:)));
