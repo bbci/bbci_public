a1c7bc6a90881dc14b72a5f1044a78177622d8d3
Stephanie <stephanie@terra.ml.tu-berlin.de>
2016-07-20 12:53:32 +0200

    problems if cov does not have full rank

--- processing/utils/score_medianvar.m
+++ processing/utils/score_medianvar.m
@@ -1,8 +1,11 @@
-
 function score = score_medianvar(dat,W,~)
 %SCORE_MEDIANVAR - Median variance of projected components as score
 
 nChans = size(dat.x,2);
+
+if size(W,2)<nChans
+    nChans=size(W,2);
+end
 
 fv = proc_linearDerivation(dat,W);
 fv = proc_variance(fv);
